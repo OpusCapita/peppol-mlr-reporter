1e3b9c323827bbada870fb12a021b9bf418aedd1
thebilge <ibrahimbilge7@gmail.com>
2019-06-17 14:40:05 +0200

    added thread pool async executor

--- src/main/java/com/opuscapita/peppol/mlrreporter/MlrReporterApp.java
+++ src/main/java/com/opuscapita/peppol/mlrreporter/MlrReporterApp.java
@@ -8,12 +8,16 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @EnableAsync
 @SpringBootApplication
@@ -53,6 +57,18 @@ public class MlrReporterApp {
     @Bean
     public Queue queue() {
         return new Queue(queueIn);
+    }
+
+    @Bean
+    @Qualifier("a2aAsyncExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(100);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("PeppolMlrReporter-");
+        executor.initialize();
+        return executor;
     }
 
 }
