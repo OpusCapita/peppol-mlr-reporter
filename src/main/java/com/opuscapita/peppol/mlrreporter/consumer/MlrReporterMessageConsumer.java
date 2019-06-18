d2c0730877762b6ca1a13c38f4fc7d8a7b2e9094
thebilge <ibrahimbilge7@gmail.com>
2019-06-18 09:31:36 +0200

    added thread pool async executor

--- src/main/java/com/opuscapita/peppol/mlrreporter/consumer/MlrReporterMessageConsumer.java
+++ src/main/java/com/opuscapita/peppol/mlrreporter/consumer/MlrReporterMessageConsumer.java
@@ -2,6 +2,7 @@ package com.opuscapita.peppol.mlrreporter.consumer;
 
 import com.opuscapita.peppol.commons.container.ContainerMessage;
 import com.opuscapita.peppol.commons.eventing.TicketReporter;
+import com.opuscapita.peppol.commons.queue.MessageQueue;
 import com.opuscapita.peppol.commons.queue.consume.ContainerMessageConsumer;
 import com.opuscapita.peppol.mlrreporter.creator.MlrReportCreator;
 import com.opuscapita.peppol.mlrreporter.creator.MlrType;
@@ -12,21 +13,32 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.task.TaskRejectedException;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @Component
 public class MlrReporterMessageConsumer implements ContainerMessageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(MlrReporterMessageConsumer.class);
 
+    @Value("${peppol.mlr-reporter.queue.in.name}")
+    private String queueIn;
+
+    private MessageQueue messageQueue;
     private MlrReportSender mlrSender;
     private MlrReportCreator mlrCreator;
     private TicketReporter ticketReporter;
     private MlrTypeIdentifier typeIdentifier;
 
     @Autowired
-    public MlrReporterMessageConsumer(MlrReportCreator mlrCreator, MlrReportSender mlrSender,
+    public MlrReporterMessageConsumer(MessageQueue messageQueue, MlrReportCreator mlrCreator, MlrReportSender mlrSender,
                                       TicketReporter ticketReporter, MlrTypeIdentifier typeIdentifier) {
+        this.messageQueue = messageQueue;
         this.mlrSender = mlrSender;
         this.mlrCreator = mlrCreator;
         this.ticketReporter = ticketReporter;
@@ -47,13 +59,33 @@ public class MlrReporterMessageConsumer implements ContainerMessageConsumer {
             }
 
             String report = mlrCreator.create(cm, type);
-            mlrSender.send(cm, report, type);
+
+            try {
+                mlrSender.send(cm, report, type);
+            } catch (Exception e) {
+                if (e instanceof TaskRejectedException || e instanceof RejectedExecutionException) {
+                    sendToRetry(cm);
+                } else {
+                    throw e;
+                }
+            }
 
         } catch (Exception e) {
             String message = "Error occurred while creating MLR report for file: " + cm.getFileName();
             ticketReporter.reportWithContainerMessage(cm, e, message);
             logger.error(message, e);
         }
+    }
+
+    private void sendToRetry(ContainerMessage cm) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+                messageQueue.convertAndSend(queueIn, cm);
+            } catch (IOException | TimeoutException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 }
