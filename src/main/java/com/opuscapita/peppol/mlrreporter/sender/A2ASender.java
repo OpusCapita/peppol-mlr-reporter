6e6598b157a64c68034a097c4aeb117d83c5c8ca
thebilge <ibrahimbilge7@gmail.com>
2019-06-18 17:14:00 +0200

    retry if connection fails to a2a endpoint

--- src/main/java/com/opuscapita/peppol/mlrreporter/sender/A2ASender.java
+++ src/main/java/com/opuscapita/peppol/mlrreporter/sender/A2ASender.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -29,22 +30,40 @@ public class A2ASender {
     public void send(String report, String fileName) {
         logger.info("A2ASender.send called for file: " + fileName + " in thread: " + Thread.currentThread().getName());
 
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Transfer-Encoding", "chunked");
-            headers.set("Document-Path", String.format("/mlr/%s", fileName));
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.set("Authorization", config.getAuthHeader());
-            HttpEntity<Resource> entity = new HttpEntity<>(new ByteArrayResource(report.getBytes()), headers);
-            logger.debug("Wrapped and set the request body as file");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Transfer-Encoding", "chunked");
+        headers.set("Document-Path", String.format("/mlr/%s", fileName));
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.set("Authorization", config.getAuthHeader());
+        HttpEntity<Resource> entity = new HttpEntity<>(new ByteArrayResource(report.getBytes()), headers);
+        logger.debug("Wrapped and set the request body as file");
 
-            ResponseEntity<String> result = restTemplate.exchange(config.host, HttpMethod.POST, entity, String.class);
-            logger.info("MLR successfully sent to A2A, filename: " + fileName + ", got response: " + result.toString());
+        // this method shouldn't throw exception because of @Async annotation
+        // that is why I added this ugly retry-once-more logic for connection exceptions
+        try {
+            sendRequest(fileName, entity);
         } catch (Exception e) {
-            String message = "Error occurred while trying to send the MLR to A2A: " + fileName;
-            ticketReporter.reportWithoutContainerMessage(null, fileName, e, message);
-            logger.error(message, e);
+            if (e instanceof ResourceAccessException) {
+                try {
+                    sendRequest(fileName, entity);
+                } catch (Exception e2) {
+                    handleError(fileName, e2);
+                }
+            } else {
+                handleError(fileName, e);
+            }
         }
+    }
+
+    private void sendRequest(String fileName, HttpEntity<Resource> entity) {
+        ResponseEntity<String> result = restTemplate.exchange(config.host, HttpMethod.POST, entity, String.class);
+        logger.info("MLR successfully sent to A2A, filename: " + fileName + ", got response: " + result.toString());
+    }
+
+    private void handleError(String fileName, Exception e) {
+        String message = "Error occurred while trying to send the MLR to A2A: " + fileName;
+        ticketReporter.reportWithoutContainerMessage(null, fileName, e, message);
+        logger.error(message, e);
     }
 
 }
