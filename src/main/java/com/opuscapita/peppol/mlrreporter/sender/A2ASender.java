7a7d56a3d00f761cd4411d99cad2679d9ad70767
thebilge <ibrahimbilge7@gmail.com>
2019-06-21 10:11:45 +0200

    removing async again

--- src/main/java/com/opuscapita/peppol/mlrreporter/sender/A2ASender.java
+++ src/main/java/com/opuscapita/peppol/mlrreporter/sender/A2ASender.java
@@ -1,15 +1,15 @@
 package com.opuscapita.peppol.mlrreporter.sender;
 
-import com.opuscapita.peppol.commons.eventing.TicketReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Component
 public class A2ASender {
@@ -18,26 +18,15 @@ public class A2ASender {
 
     private final A2AConfiguration config;
     private final RestTemplate restTemplate;
-    private final TicketReporter ticketReporter;
 
-    public A2ASender(RestTemplate restTemplate, A2AConfiguration config, TicketReporter ticketReporter) {
+    public A2ASender(RestTemplate restTemplate, A2AConfiguration config) {
         this.config = config;
         this.restTemplate = restTemplate;
-        this.ticketReporter = ticketReporter;
     }
 
-    @Async("a2aAsyncExecutor")
-    public void send(String report, String fileName) {
-        logger.info("A2ASender.send called for file: " + fileName + " in thread: " + Thread.currentThread().getName());
+    void send(String report, String fileName) throws IOException {
+        logger.debug("Sending A2A mlr request for file: " + fileName);
 
-        try {
-            sendRequest(report, fileName);
-        } catch (Exception e) {
-            handleError(fileName, e);
-        }
-    }
-
-    private void sendRequest(String report, String fileName) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Transfer-Encoding", "chunked");
         headers.set("Document-Path", String.format("/mlr/%s", fileName));
@@ -49,20 +38,9 @@ public class A2ASender {
         try {
             ResponseEntity<String> result = restTemplate.exchange(config.host, HttpMethod.POST, entity, String.class);
             logger.info("MLR successfully sent to A2A, filename: " + fileName + ", got response: " + result.toString());
-        } catch (ResourceAccessException e) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignored) {
-            }
-            // retry connection issues..
-            sendRequest(report, fileName);
+        } catch (Exception e) {
+            throw new IOException("Error occurred while trying to send the MLR to A2A", e);
         }
-    }
-
-    private void handleError(String fileName, Exception e) {
-        String message = "Error occurred while trying to send the MLR to A2A: " + fileName;
-        ticketReporter.reportWithoutContainerMessage(null, fileName, e, message);
-        logger.error(message, e);
     }
 
 }
