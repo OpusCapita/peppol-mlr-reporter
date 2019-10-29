f50e372c97c125148c7dcc5b49dac6d58e30aca7
thebilge <ibrahimbilge7@gmail.com>
2019-10-29 14:15:17 +0100

    config changes, peppol-commons update

--- src/main/java/com/opuscapita/peppol/mlrreporter/sender/SiriusSender.java
+++ src/main/java/com/opuscapita/peppol/mlrreporter/sender/SiriusSender.java
@@ -1,6 +1,5 @@
 package com.opuscapita.peppol.mlrreporter.sender;
 
-import com.opuscapita.peppol.commons.storage.Storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
